dee2bf25872d326213a337aa7a73f9aed18b2ae3
Chris <c.chriv@gmail.com>
2014-01-06 19:31:03 +0100

    exo5

--- Wagon-0/04. Age in days/age_in_days.rb
+++ Wagon-0/04. Age in days/age_in_days.rb
@@ -1,7 +1,13 @@
+
 def age_in_days(day, month, year)
-  # your code goes here !
+	
+	t = Time.new 
+	t2 = Time.now		
+
+	puts "You have #{(t2.tv_sec-t.tv_sec)/(60*60*24)} days buddy"
+
 end
 
 
 # Testing your code
-puts age_in_days(6, 11, 1985)
+puts age_in_days(26, 02, 1988)
--- Wagon-0/05. What's your name ?/introduce_yourself.rb
+++ Wagon-0/05. What's your name ?/introduce_yourself.rb
@@ -1,1 +1,32 @@
 # your code goes here, introduce yourself !
+=begin
+
+puts "Quel est ton prenom ?"
+	prenom = gets.chomp
+
+puts "Quel est ton deuxieme prenom ?"
+	middle = gets.chomp
+
+puts "Quel est ton nom de famille ?"
+	nom = gets.chomp
+
+	puts "Hello, " + prenom + " " + middle + " " + nom + " !"
+
+=end
+
+=begin
+
+puts "Quel est ton prenom ?"
+	prenom = gets.chomp
+
+puts "Quel est ton deuxieme prenom ?"
+	middle = gets.chomp
+
+puts "Quel est ton nom de famille ?"
+	nom = gets.chomp
+
+	puts "Hello, #{prenom} #{middle} #{nom} !"
+
+=end
+
+
