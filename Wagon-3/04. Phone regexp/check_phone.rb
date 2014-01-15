1238f8557a3b497d6729bb7c8cafe077f37fa992
Chris <c.chriv@gmail.com>
2014-01-15 15:40:53 +0100

    first commit

--- Wagon-3/04. Phone regexp/check_phone.rb
+++ Wagon-3/04. Phone regexp/check_phone.rb
@@ -1,9 +1,35 @@
+
 def is_valid_phone_number(phone_number)
 	final_number = phone_number.delete " +-"
 	final_number =~ /(^0|33|00)(\d{9,12})/ ? true : false
 end
 
+=begin
+def is_valid_phone_number(phone_number)
+	final_number = phone_number.delete " +-"
 
+	if final_number.length == 9
+		#631749000
+		final_number =~ /\d/ ? true : false
+	end
+	
+	if final_number.length == 10
+		#0631749000
+		final_number =~ /^0/ ? true : false
+	end
+
+	if final_number.length == 11
+		#33631749000
+		final_number =~ /^33/ ? true : false
+	end
+
+	if final_number.length == 13
+		#0033631749000
+		final_number =~ /^0033/ ? true : false
+	end
+
+end
+=end
 
 # tests
 
@@ -14,3 +40,4 @@ puts is_valid_phone_number("+33 6 65 36 36 36") # should output true
 
 puts is_valid_phone_number("06 65 36 36") # should output false
 puts is_valid_phone_number("2336653636") # should output false
+puts is_valid_phone_number("0021631749000") # should output false
--- Wagon-3/05. csv parsing/parsing_recipes.rb
+++ Wagon-3/05. csv parsing/parsing_recipes.rb
@@ -1,3 +1,59 @@
-require 'csv'
+def cookbook
 
-# your code goes here 
+	require 'csv'
+
+	array_of_recipes = CSV.read("recipes.csv")
+
+	#Categories 
+	categories = []
+	array_of_recipes.each { |a| categories << a[1] }
+	categories = categories.uniq
+
+	puts "We have recipes for : #{categories}"
+	puts "Which category are you interested in ?"
+	answer = gets.chomp
+
+		if categories.include?(answer)
+		puts " "
+		puts "Ok ! For the category #{answer}, we have : " 
+		
+		else
+		puts " "
+		puts "Sorry Sir. We do not have any recipes for this category (have you been careful with the case ?)."
+		puts "Choose another one."
+		end
+
+	# Available recipes
+		array_of_recipes.each do |recette|
+			if recette.include?(answer)
+				puts "- " + recette[0]
+			else 
+			end
+		end	
+
+	# Now ?
+	puts " "
+	puts "Now which recipe do you want ?"
+	chosen_one = gets.chomp
+
+	array_of_recipes.each do |x|
+			case true
+			when x[0] == chosen_one
+				difficulty = x[4]
+				preparation = x[2]
+				cooking = x[3]
+
+					puts " "
+					puts chosen_one + " !" + " What a marvelous idea."
+					puts "************************************"
+					puts "difficulty : #{difficulty}"
+					puts "preparation : #{preparation} min"
+					puts "cooking : #{cooking}h"
+					puts "************************************"
+
+			else
+			end
+		end
+end
+
+cookbook
